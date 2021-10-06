2297381b05253045ded6c020e0dcbaef110a541b
yihuaf <yihuaf@unkies.org>
2021-10-06 07:38:32 +0200

    address review

--- src/runtime/test.rs
+++ src/runtime/test.rs
@@ -34,5 +34,6 @@ fn test_linux_device_cgroup_to_string() {
 fn test_load_sample_spec() {
     let fixture_path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("src/runtime/test/fixture/sample.json");
-    Spec::load(fixture_path).unwrap();
+    let err = Spec::load(fixture_path);
+    assert!(err.is_ok(), "failed to load spec: {:?}", err);
 }
